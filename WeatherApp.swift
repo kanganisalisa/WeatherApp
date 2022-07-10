ba19bc363974728a72c3f35b7a0b52aac79a337d
Alisa Kanganis <99622944+kanganisalisa@users.noreply.github.com>
2022-07-10 14:44:07 -0400

    Add files via upload

---
+++ WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Alisa Kanganis on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherButton.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+import Foundation
+
+struct WeatherButton: View {
+    var title:String
+    var textColor:Color
+    var backgroundColor:Color
+    var body: some View {
+        Text(title)
+            .frame(width: 200, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
